a602a57c537b9f3943d081d4042c8571c1c8ffa5
adishap <porwaladisha@gmail.com>
2019-08-06 09:03:54 +0530

    body UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import LazyLoad from 'react-lazy-load';
 import { MoviesData } from '../fixture/completeMoviesData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes, faThumbsUp, faThumbsDown, faPlayCircle, faCalendar, faQuestion } from '@fortawesome/free-solid-svg-icons'
 
 const FetchStatus = {
     LOADING: 'LOADING',
@@ -45,7 +47,6 @@ class Body extends Component {
     }
 
     renderTrailerData() {
-        let trailerColumns = this.state.trailerColumns;
         let showTrailerData = this.state.showTrailerData;
         let imageURL = "https://in.bmscdn.com/events/moviecard/" + showTrailerData.movieData.EventCode + ".jpg";
         let trailerURL = showTrailerData.movieData.TrailerURL;
@@ -53,38 +54,57 @@ class Body extends Component {
         trailerURL[3] = trailerURL[3].split('=')[1];
         trailerURL.splice(3, 0, "embed");
         trailerURL = trailerURL.join('/');
+        
+        let movieGenre = [];
+        showTrailerData.movieData.EventGenre.split('|').forEach(_ => {
+            movieGenre.push(<div key={_} className='tag'>{_}</div>);
+        });
         return (
-            <div style={{display: 'flex'}}>
-                <div style={{display: 'flex', width: trailerColumns * 250, marginBottom: 15}}>
-                    <div style={{width: "60%", marginLeft: 10, borderRadius: "10px 0 0 10px"}}>
+            <div key={showTrailerData.movieData.EventCode} className='row'>
+                <div className='trailer-wrapper' style={{width: this.state.trailerColumns * 250}}>
+                    <div className='trailer'>
                         <iframe title={showTrailerData.movieData.EventCode} style={{width: '100%', height: 500, borderRadius: "10px 0 0 10px", verticalAlign: 'bottom', border: 0}}
                             src={trailerURL + "?autoplay=1"}
                         />
                     </div>
-                    <div style={{width: "40%", marginRight: 9, borderRadius: "0 10px 10px 0", textAlign: 'left', padding: 10, position: 'relative'}}>
-                            {showTrailerData.movieData.EventDimension}
-                            {showTrailerData.movieData.EventName}
-                            {showTrailerData.movieData.DispReleaseDate}
-                        <div style={{display: 'flex', alignItems: 'center'}}>
-                                {showTrailerData.movieData.wtsPerc + ' % | ' + showTrailerData.movieData.wtsCount + " Votes"}
+                    <div className='movie-details'>
+                        <div className='movie-title'>{showTrailerData.movieData.EventName}</div>
+                        <div className='language'>{showTrailerData.movieData.EventLanguage}</div>
+                        <div className='movie-info'>
+                            <div>
+                                <FontAwesomeIcon icon={faThumbsUp} />
+                            </div>
+                            <div>
+                                {`${showTrailerData.movieData.wtsPerc} %`} <br />
+                                <small>{`${showTrailerData.movieData.wtsCount} Votes`}</ small>
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faCalendar} />
+                            </div>
+                            <div>
+                                {showTrailerData.movieData.ShowDate.split(', ')[0]} <br />
+                                <small>{showTrailerData.movieData.ShowDate.split(', ')[1]}</small>
+                            </div>
                         </div>
-                            {showTrailerData.movieData.EventGenre.split('|')[0]}
-                            {showTrailerData.movieData.EventLanguage}
-                        <div style={{display: 'flex', padding: "0.5%", textAlign: 'center', position: 'absolute', width: "100%", right: 0, bottom: 10}}>
-                            <div style={{width: '33%'}}>
-                                    WILL WATCH
+                        {movieGenre}
+                        <div className='user-review'>
+                            <div className='will-watch'>
+                                <FontAwesomeIcon icon={faThumbsUp} />
+                                WILL WATCH
                             </div>
-                            <div style={{width: '33%'}}>
-                                    MAY BE
+                            <div className='maybe-watch'>
+                                <FontAwesomeIcon icon={faQuestion} />
+                                MAY BE
                             </div>
-                            <div style={{width: '33%'}}>
-                                    WON'T WATCH
+                            <div className='wont-watch'>
+                                <FontAwesomeIcon icon={faThumbsDown} />
+                                WON'T WATCH
                             </div>
                         </div>
-                        <div style={{backgroundImage: `url(${imageURL})`, position: 'absolute', top: 0, right: 0, height: "100%", width: "100%", opacity: 0.1, backgroundSize: "cover", borderRadius: "0 10px 10px 0"}}></div>
-                        <div style={{cursor: 'pointer', position: 'absolute', top: 10, right: 10}}
-                             onClick={() => this.closeTrailer()}>
-                            close
+                        <div className='trailer-img' style={{backgroundImage: `url(${imageURL})`}}></div>
+                        <div className='close'
+                            onClick={() => this.closeTrailer()}>
+                            <FontAwesomeIcon icon={faTimes} />
                         </div>
                     </div>
                 </div>
@@ -113,33 +133,27 @@ class Body extends Component {
         let eventId = listOfMovieIds[rowNumber * trailerColumns + columnNumber];
         let movieData = this.state.completeMoviesData[1][eventId];
         return (
-            <div key={eventId} style={{width: 232, margin: "0 9px 15px 9px", borderRadius: 10, cursor: 'pointer'}}
-                 onClick={() => this.openTrailer(rowNumber, movieData)}>
-                <div style={{position: 'relative'}}>
-                    <LazyLoad width={232} height={300}>
-                        <img alt={eventId} src={"https://in.bmscdn.com/events/moviecard/" + eventId + ".jpg"} style={{borderRadius: "10px 10px 0 0"}}/>
+            <div key={eventId} className='movie-wrapper'
+                onClick={() => this.openTrailer(rowNumber, movieData)}>
+                <div>
+                    <LazyLoad>
+                        <img alt={eventId} src={`https://in.bmscdn.com/events/moviecard/${eventId}.jpg`}/>
                     </LazyLoad>
-                    
-                    <div style={{position: 'absolute', top: 0, left: 0, borderRadius: "10px 10px 0 0", margin: 10}}>
-                            {movieData.EventDimension}
+                    <div className='date-wrapper'>
+                        {movieData.ShowDate.split(', ')[0].split(' ')[1]} <br />
+                        {movieData.ShowDate.split(', ')[1]}
                     </div>
-                    <div style={{position: 'absolute', top: 0, right: 0, borderRadius: "10px 10px 0 0", margin: 10}}>
-                        <div style={{padding: 10, width: 42, borderRadius: 62}}>
-                                {movieData.ShowDate.split(', ')[0].split(' ')[1]}
-                                {movieData.ShowDate.split(', ')[1]}
-                        </div>
-                    </div>
-                    <div style={{position: 'absolute', bottom: 0, right: 0, borderRadius: "10px 10px 0 0", margin: 10, textAlign: 'right'}}>
-                            {movieData.EventGenre.split('|')[0]}
-                            {movieData.EventLanguage}
-                    </div>
-                    <div style={{position: 'absolute', bottom: 0, left: 0, borderRadius: "10px 10px 0 0", padding: 10}}>
-                        <div style={{display: 'flex', alignItems: 'center'}}>
-                                {movieData.wtsPerc + ' %'}
+                    <div className='upvote-info'>
+                        <div>
+                            <FontAwesomeIcon icon={faThumbsUp} />
+                            {movieData.wtsPerc + ' %'}
                         </div>
                         <div>
-                                {movieData.wtsCount + " Votes"}
+                            <small>{`${movieData.wtsCount} Votes`}</small>
                         </div>
+                    </div>
+                    <div className='play-icon'>
+                        <FontAwesomeIcon icon={faPlayCircle} />
                     </div>
                 </div>
                     {movieData['EventName']}
@@ -153,7 +167,7 @@ class Body extends Component {
             rowData.push(this.renderMovie(rowNumber, i, trailerColumns, listOfMovieIds))
         }
         return (
-            <div key={rowNumber} style={{display: 'flex'}}>
+            <div key={rowNumber} className='row'>
                 {rowData}
             </div>
         );
@@ -162,7 +176,7 @@ class Body extends Component {
     render() {
         if (this.state.currentState === FetchStatus.LOADING) {
             return (
-                <div style={{textAlign: 'center'}}>
+                <div className='loading'>
                     Loading
                 </div>
             );
@@ -172,7 +186,6 @@ class Body extends Component {
         let trailerColumns = this.state.trailerColumns;
         let numberOfRows = Math.floor(listOfMovieIds.length / trailerColumns);
         let moviesData = [];
-        console.log(numberOfRows, listOfMovieIds.length);
         for (let i = 0; i < numberOfRows; i++) {
             if (this.state.isShowTrailer && this.state.showTrailerData.rowNumber === i) {
                 let trailerData = this.renderTrailerData();
@@ -182,9 +195,9 @@ class Body extends Component {
         }
 
         return (
-            <div>
+            <div className='body'>
                 <LazyLoad>
-                    <div style={{paddingTop: 15}}>
+                    <div>
                         {moviesData}
                     </div>
                 </LazyLoad>
